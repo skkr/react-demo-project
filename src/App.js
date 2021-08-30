bbca329d37296f4085824221b59dea592cebd3c2
Sikker <nico.sikker@gmail.com>
2021-08-29 22:18:33 -0300

    Get data from API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import logo from './logo.svg';
+import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+
+  const getData = async () => {
+    const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
+    console.log(res.data);
+  }
+
+  useEffect(() => {
+    getData();
+  }, [])
+
   return (
     <div className="App">
       <h1>Coin Market</h1>
