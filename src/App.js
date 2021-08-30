9bba87208d4c3e050c9d174538d7e742efd4d3a1
Sikker <nico.sikker@gmail.com>
2021-08-29 23:59:09 -0300

    Enabled coin search and design improvements.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Coins from './components/Coins';
 function App() {
 
   const [coins, setCoins] = useState([]);
+  const [search, setSearch] = useState('');
 
   const getData = async () => {
     const res = await axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false');
@@ -19,8 +20,24 @@ function App() {
 
   return (
     <div className="App bg-dark">
-      <h1 className="text-light">Coin Market</h1>
-      <Coins coins={coins}/>
+
+      <h1 className="text-light bg-dark bg-gradient py-1 my-0 ">Crypto-Market</h1>
+
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <input type="text" placeholder="Search a coin"
+              className="form-control bg-dark text-light border-secondary my-3 text-center"
+              onChange={ e => (setSearch(e.target.value))}
+            />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <Coins coins={coins} search={search}/>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/components/Coins.jsx
+++ src/components/Coins.jsx
@@ -1,22 +1,27 @@
 import React from 'react';
 import Rows from "./Rows";
 
-const Coins = ({coins}) => {
+const titles = ['#', 'Coin', 'Price', 'Price change', '24h volume'];
 
-  const titles = ['#', 'Coin', 'Price', 'Price change', '24h volume'];
+const Coins = ({coins, search}) => {
+
+  const filteredCoins = coins.filter( coin =>
+    coin.name.toLowerCase().includes(search.toLowerCase()) ||
+    coin.symbol.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
       <table className="table table-dark table-hover ">
         <thead>
           <tr>
 
-          {titles.map( title => (
-            <th>{title}</th>
+          {titles.map( (title, index) => (
+            <th key={index}>{title}</th>
           ))}
           </tr>
         </thead>
         <tbody>
-          {coins.map( (coin, index) => (
+          {filteredCoins.map( (coin, index) => (
             <Rows coin={coin} index={index} key={index} />
           ))}
         </tbody>
